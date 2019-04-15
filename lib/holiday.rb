82840d62d9794b91465e9f95b7ec9e00a70f1048
Won Kim <wonsae.kim@gmail.com>
2019-04-15 03:49:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == :winter
       holiday.each do |v|
-        
+        v.push(supply)
       end
     end
   end
