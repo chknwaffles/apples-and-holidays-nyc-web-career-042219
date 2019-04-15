9a800da9a09cacd07e25b29d890b74e31e7f3451
Won Kim <wonsae.kim@gmail.com>
2019-04-15 03:51:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,8 +27,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday|
     if season == :winter
-      holiday.each do |v|
-        v.push(supply)
+      holiday_hash[season][:christmas].push(supply)
+      holiday_hash[season][:new_years].push(supply)
       end
     end
   end
