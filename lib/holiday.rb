5deefd3a94c533e475d515ef5a629402e2dfc491
Won Kim <wonsae.kim@gmail.com>
2019-04-15 04:29:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,6 +75,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
+    msg = ""
     season_format = season.to_s.capitalize
     puts "#{season_format}:"
     holiday.each do |holiday, list|
