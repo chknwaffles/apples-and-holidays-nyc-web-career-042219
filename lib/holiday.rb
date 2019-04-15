d9d91e404feb02919590b2d0f9947f9fb2855048
Won Kim <wonsae.kim@gmail.com>
2019-04-15 03:56:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,6 +49,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # remember to return the updated hash
   temp_hash = {season: {holiday_name: supply_array}}
   holiday_hash.merge(temp_hash)
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
